71e9596d8b9377bcfce31767aea09ea3187bb7be
Udot Andrii <udot.dron@gmail.com>
2019-08-28 19:38:06 +0300

    lesson 50

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,12 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/1551511784_10.jpg'
+
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'http://stuki-druki.com/biofoto/Dmitriy-Egorov-3-01.jpg',
-                followed: true,
-                fullName: 'Andrew',
-                status: 'I am a BOSS!!!',
-                location: {
-                    city: 'Kharkiv',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/267px-Dmitry_Nagiev_2017_4.jpg',
-                followed: true,
-                fullName: 'Andrew',
-                status: 'I am a BOSS!!!',
-                location: {
-                    city: 'Kharkiv',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/22/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B0%D1%80%D1%8C%D1%8F%D0%BD%D0%BE%D0%B2_%28%D0%B0%D0%BA%D1%82%D1%91%D1%80%29.jpg/260px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B0%D1%80%D1%8C%D1%8F%D0%BD%D0%BE%D0%B2_%28%D0%B0%D0%BA%D1%82%D1%91%D1%80%29.jpg',
-                followed: false,
-                fullName: 'Petr',
-                status: 'I am too...',
-                location: {
-                    city: 'Moscow',
-                    country: 'RAssia'
-                }
-            },
-            {
-                id: 4,
-                photoUrl: 'http://stuki-druki.com/biofoto/Dmitriy-Muhin-01.jpg',
-                followed: true,
-                fullName: 'Diana',
-                status: 'I am beautiful',
-                location: {
-                    city: 'San-Francisco',
-                    country: 'United State'
-                }
-            }
-        ])
+    if (props.user.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items));
     }
     return (
         <div>
@@ -57,7 +15,7 @@ let Users = (props) => {
                     <span>
                         <div>
                             <img className={styles.userPhoto}
-                                 src={u.photoUrl} alt="Аватарка"/>
+                                 src={u.photos.small !=null ? u.photos.small: userPhoto} alt="Аватарка"/>
                         
                         </div>
                         <div>
@@ -77,10 +35,10 @@ let Users = (props) => {
                         </span>
                         <span>
                             <div>
-                                {u.location.city}
+                                {'u.location.city'}
                             </div>
                             <div>
-                                {u.location.country}
+                                {'u.location.country'}
                             </div>
                         </span>
                     </span>
