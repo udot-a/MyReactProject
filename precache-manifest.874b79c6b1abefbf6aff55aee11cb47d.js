37d94fb6334e3bf21f6414a3006fe44978816d3e
Udot Andrii <udot.dron@gmail.com>
2019-11-27 15:37:56 +0200

    Updates

---
+++ precache-manifest.874b79c6b1abefbf6aff55aee11cb47d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5411ab3239acb2df0891f8786348ec",
+    "url": "/MyReactProject/index.html"
+  },
+  {
+    "revision": "bf59cd7cb951ce69775f",
+    "url": "/MyReactProject/static/css/main.ce74dbde.chunk.css"
+  },
+  {
+    "revision": "f7a1c1c1771d85395067",
+    "url": "/MyReactProject/static/js/2.c54b357d.chunk.js"
+  },
+  {
+    "revision": "bf59cd7cb951ce69775f",
+    "url": "/MyReactProject/static/js/main.0cc1e8c2.chunk.js"
+  },
+  {
+    "revision": "e33d56b9b43b2c9f11ee",
+    "url": "/MyReactProject/static/js/runtime~main.d85ecdb3.js"
+  },
+  {
+    "revision": "2096574fdb2c45187bff40aeef18a10d",
+    "url": "/MyReactProject/static/media/Pacman-1s-200px.2096574f.svg"
+  },
+  {
+    "revision": "4802d74b61e6536af5fc5cc6c51189b6",
+    "url": "/MyReactProject/static/media/user.4802d74b.jpg"
+  }
+]);
