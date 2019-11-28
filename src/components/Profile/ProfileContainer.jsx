e125defaf1104741c0ed88bbff7fd5646ebd4809
Udot Andrii <udot.dron@gmail.com>
2019-11-28 16:45:48 +0200

    'lesson-69HOC'

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/profile-reducer";
 import {Redirect, withRouter} from "react-router-dom";
-import {usersAPI} from "../../api/api";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+
+
 class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         (!userId) && (userId = 2);
         this.props.getUserProfile(userId)
-        // usersAPI.getProfile(userId)
-        //     .then(response => {
-        //        this.props.setUserProfile(response.data);
-        //     });
 
     }
 
     render() {
-        if (!this.props.isAuth) return <Redirect to={'/login'}/>
         return (
             <Profile {...this.props} profile = {this.props.profile}/>
         );
     }
 }
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
+
 
 let mapStateToProps = (state) =>({
     profile:state.profilePage.profile,
-    isAuth: state.auth.isAuth
 })
 
-let WithUrlDataContainerComponent = withRouter(ProfileContainer)
+let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent)
 export default connect(mapStateToProps,{
     getUserProfile
 })(WithUrlDataContainerComponent);
