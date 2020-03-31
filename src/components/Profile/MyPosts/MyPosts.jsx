b353c5ab6cdf27b67d926a302502134982601258
Udot Andrii <udot.dron@gmail.com>
2020-03-31 16:54:56 +0300

    'lesson88'

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,42 +2,33 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post'
 import {Field, reduxForm} from "redux-form";
-import {
-    maxLengthCreator,
-    minLengthCreator,
-    required
-} from "../../../utils/validators/validators";
+import {maxLengthCreator, minLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormControls/FormsControls";
 
 const maxLength30 = maxLengthCreator(30)
 const minLength10 = minLengthCreator(10)
 
-class MyPosts extends React.Component {
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps !=this.props || nextState !=this.state;
+const MyPosts = React.memo(props => {
+    let count = 0
+    let postsElements =
+        [...props.posts].reverse().map(p => (
+            <Post key={count++} message={p.message} picture={p.picture} LikeCounter={p.likesCounter}/>));
+
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
     }
-    render() {
-        let count = 0
-        let postsElements =
-            this.props.posts.map(p => (
-                <Post key={count++} message={p.message} picture={p.picture} LikeCounter={p.likesCounter}/>));
-
-        let onAddPost = (values) => {
-            this.props.addPost(values.newPostText);
-        }
-
-        return (
-            <div className={s.postsBlock}>
-                <h3>My posts</h3>
-                <div className={s.posts}>
-                    {postsElements}
-                    <AddNewPostForm onSubmit={onAddPost}/>
-                </div>
+console.log('RENDER YO')
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div className={s.posts}>
+                {postsElements}
+                <AddNewPostForm onSubmit={onAddPost}/>
             </div>
-        );
-    }
-}
+        </div>
+    );
+});
 
 let AddNewPostForm =(props)=>{
     return (
